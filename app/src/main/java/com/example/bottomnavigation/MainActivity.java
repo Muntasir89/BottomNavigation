af6bf9eae2ab50f673b98da05e2a58d0b24cca74
Muntasir89 <montachirhossen76@gmail.com>
2021-02-07 20:08:53 +0600

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.bottomnavigation;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+public class MainActivity extends AppCompatActivity{
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.BottomBarId);
+
+        if(savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.FrameViewId, new HomeSubActivity()).commit();
+        }
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                if(item.getItemId()==R.id.HomeId){
+                    fragment = new HomeSubActivity();
+                }
+                if(item.getItemId()==R.id.SettingId){
+                    fragment = new SettingSubActivity();
+                }
+                if(item.getItemId()==R.id.ProfileId){
+                    fragment = new ProfileSubActivity();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.FrameViewId, fragment).commit();
+
+                return false;
+            }
+        });
+    }
+}
